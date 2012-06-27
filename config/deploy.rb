227e3c4a88aab3a144bcceffdc0609cb6cfd30e7
Josh Lin <joshlin@uw.edu>
2012-06-27 12:13:00 -0700

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,9 @@
+require 'bundler/capistrano'
 require 'mongrel_cluster/recipes'
+
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#set :rvm_type, :user
+#require "rvm/capistrano"
 
 set :application, "expo"
 set :repository,  "git@github.com:uwexpd/expo.git"
@@ -6,14 +11,20 @@ set :repository,  "git@github.com:uwexpd/expo.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+default_run_options[:pty] = true
+#set :branch, "master"
+set :deploy_via, :remote_cache
+
 set :user, "joshlin"
 set :runner, "root"
 set :use_sudo, true
 set :deploy_to, "/usr/local/apps/#{application}"
+#set :deploy_to, "/home/joshlin/#{application}" # for testing
 
-role :web, "expo.uaa.washington.edu"                          # Your HTTP server, Apache/etc
-role :app, "expo.uaa.washington.edu"                          # This may be the same as your `Web` server
-role :db,  "expo.uaa.washington.edu", :primary => true # This is where Rails migrations will run
+server "expo.uaa.washington.edu", :app, :web, :db, :primary => true
+#role :web, "expo.uaa.washington.edu"                          # Your HTTP server, Apache/etc
+#role :app, "expo.uaa.washington.edu"                          # This may be the same as your `Web` server
+#role :db,  "expo.uaa.washington.edu", :primary => true        # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
