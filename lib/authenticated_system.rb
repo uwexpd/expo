07e37c95390f7c37f50af7bc40583da2ac7d5301
Josh Lin <joshlin@uw.edu>
2012-08-16 15:58:22 -0700

    change for shibboleth

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -176,9 +176,12 @@ module AuthenticatedSystem
   private
     @@http_auth_headers = %w(X-HTTP_AUTHORIZATION HTTP_AUTHORIZATION Authorization)
     # gets BASIC auth info
+    # def get_auth_data
+    #       auth_key  = @@http_auth_headers.detect { |h| request.env.has_key?(h) }
+    #       auth_data = request.env[auth_key].to_s.split unless auth_key.blank?
+    #       return auth_data && auth_data[0] == 'Basic' ? Base64.decode64(auth_data[1]).split(':')[0..1] : [nil, nil] 
+    #     end
     def get_auth_data
-      auth_key  = @@http_auth_headers.detect { |h| request.env.has_key?(h) }
-      auth_data = request.env[auth_key].to_s.split unless auth_key.blank?
-      return auth_data && auth_data[0] == 'Basic' ? Base64.decode64(auth_data[1]).split(':')[0..1] : [nil, nil] 
-    end
+         [request.env["eppn"].split("@").first, nil] rescue [nil, nil]
+    end    
 end
