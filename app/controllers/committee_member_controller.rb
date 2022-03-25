949f17be74fa4625f8fbe8d7a3db8e341013c40a
joshlin <joshlin@uw.edu>
2022-03-25 10:54:43 -0700

    add to control workshop display in symposium

--- app/controllers/committee_member_controller.rb
+++ app/controllers/committee_member_controller.rb
@@ -67,7 +67,7 @@ class CommitteeMemberController < ApplicationController
   def meetings
     session[:breadcrumbs].add "#{@committee_member.committee.meetings_title_display}"
     if params[:committee_member]
-      if ç.update_attributes(params[:committee_member])
+      if @committee_member.update_attributes(params[:committee_member])
         flash[:notice] = "All information saved."
         if @committee_member.committee.interview_offering_id.blank?
           redirect_to :action => "complete"
